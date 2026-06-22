cae2c430e9faf1301b646bfdce77c78e9f8a751a
Lucasadriel018 <aoliveira@syngularsolutions.com>
2026-06-22 09:08:50 -0300

    Update SYGA_2.0.py

--- SYGA_2.0.py
+++ SYGA_2.0.py
@@ -61,6 +61,15 @@ PAGE_CONFIG = {
     "initial_sidebar_state": "expanded",
 }
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.set_page_config(**PAGE_CONFIG)
 
 st.markdown(
